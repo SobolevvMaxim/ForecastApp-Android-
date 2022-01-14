f18d68f7417418957cefc92e48712a43c679c807
Admin <brtgfc@bk.ru>
2022-01-14 18:46:52 +0300

    deleted second activity theme added

--- app/src/main/java/com/example/forecast/feature_forecast/presentation/activities/MainPageActivity.kt
+++ app/src/main/java/com/example/forecast/feature_forecast/presentation/activities/MainPageActivity.kt
@@ -2,33 +2,35 @@ package com.example.forecast.feature_forecast.presentation.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.forecast.feature_forecast.domain.model.CityWeather
 import com.example.forecast.R
+import com.example.forecast.feature_forecast.presentation.NavigationHost
+import com.example.forecast.feature_forecast.presentation.fragments.CitiesFragment
 import com.example.forecast.feature_forecast.presentation.fragments.MainPageFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 const val P_LOG = "PARCELABLE_LOG"
 
 @AndroidEntryPoint
-class MainPageActivity : AppCompatActivity(R.layout.activity_main_page) {
+class MainPageActivity : AppCompatActivity(R.layout.activity_main_page), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         if (savedInstanceState == null) {
-            val chosenCity: CityWeather = intent.getParcelableExtra(getString(R.string.get_city_extra)) ?: CityWeather(
-                id = "",
-                name = "",
-                country = "",
-                lat = "",
-                lon = "",
-                temperatures = ArrayList(),
-                chosen = true,
-                forecastDate = "01.01.2021"
-            )
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container_main, MainPageFragment.create(city = chosenCity, city_tag = getString(R.string.get_city_extra)))
-                .commit()
+            navigateTo(CitiesFragment.create(), addToBackstack = false)
         }
+    }
+
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_main, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
