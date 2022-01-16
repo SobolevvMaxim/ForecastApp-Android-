3f6132e35bda9d0c44d75db52386f2b3fd6b1e6b
Admin <brtgfc@bk.ru>
2022-01-16 15:19:07 +0300

    simple animation added

--- app/src/main/java/com/example/forecast/feature_forecast/presentation/activities/MainPageActivity.kt
+++ app/src/main/java/com/example/forecast/feature_forecast/presentation/activities/MainPageActivity.kt
@@ -23,8 +23,13 @@ class MainPageActivity : AppCompatActivity(R.layout.activity_main_page), Navigat
     }
 
     override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
-        val transaction = supportFragmentManager
-            .beginTransaction()
+        val transaction = supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.slide_in,
+                R.anim.fade_out,
+                R.anim.fade_in,
+                R.anim.slide_out
+            )
             .replace(R.id.container_main, fragment)
 
         if (addToBackstack) {
