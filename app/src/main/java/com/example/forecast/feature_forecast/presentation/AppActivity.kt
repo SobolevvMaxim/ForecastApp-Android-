e0e94b94716e872c59ba63b5a97f184a4ab74d45
MainComp <sobolevvw@gmail.com>
2022-02-22 18:17:42 +0300

    done left swipe from main page fragment

--- app/src/main/java/com/example/forecast/feature_forecast/presentation/AppActivity.kt
+++ app/src/main/java/com/example/forecast/feature_forecast/presentation/AppActivity.kt
@@ -3,10 +3,12 @@ package com.example.forecast.feature_forecast.presentation
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
 import com.example.forecast.R
+import com.example.forecast.feature_forecast.presentation.fragments.CitiesFragment
 import com.example.forecast.feature_forecast.presentation.fragments.MainPageFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,16 +16,55 @@ const val P_LOG = "PARCELABLE_LOG"
 
 @AndroidEntryPoint
 class MainPageActivity : AppCompatActivity(R.layout.main_activity), NavigationHost,
-    ChosenCityInterface {
+    ChosenCityInterface, SwipeNavigation {
+
+    private val mDetector: GestureDetectorCompat by lazy {
+        GestureDetectorCompat(
+            this,
+            SwipeListener(this)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
-            navigateTo(MainPageFragment.create(), addToBackstack = false)
+            navigateToMainFragment()
         }
     }
 
-    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        mDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    override fun onLeftSwipe() {
+        navigateToCitiesFragment()
+    }
+
+    override fun onRightSwipe() {
+        // TODO: Correct swipe to main fragment from cities fragment
+//        navigateToMainFragment()
+    }
+
+    override fun changeChosenInBase(newChosenIndex: String) {
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString(getString(R.string.chosen_pref_key), newChosenIndex)
+            apply()
+        }
+        Log.d("CHOSEN", "changeChosenInBase: $newChosenIndex")
+    }
+
+    override fun getChosenCityID(): String {
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        val defaultValue = resources.getString(R.string.default_chosen_id)
+        val chosenIndex = sharedPref.getString(getString(R.string.chosen_pref_key), defaultValue)
+        Log.d("CHOSEN", "getChosenCItyID: $chosenIndex")
+        return chosenIndex!!
+    }
+
+    private fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
         val transaction = supportFragmentManager.beginTransaction()
             .setCustomAnimations(
                 R.anim.slide_in,
@@ -40,22 +81,11 @@ class MainPageActivity : AppCompatActivity(R.layout.main_activity), NavigationHo
         transaction.commit()
     }
 
-    override fun changeChosenInBase(newChosenIndex: String) {
-        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
-        with(sharedPref.edit()) {
-            putString(getString(R.string.chosen_pref_key), newChosenIndex)
-            apply()
-        }
-        Log.d("CHOSEN", "changeChosenInBase: $newChosenIndex")
-        Toast.makeText(this, "New chosen city index: $newChosenIndex", Toast.LENGTH_SHORT).show()
+    private fun navigateToMainFragment() {
+        navigateTo(MainPageFragment.create(), addToBackstack = false)
     }
 
-    override fun getChosenCityID(): String {
-        val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
-        val defaultValue = resources.getString(R.string.default_chosen_id)
-        val chosenIndex = sharedPref.getString(getString(R.string.chosen_pref_key), defaultValue)
-        Log.d("CHOSEN", "getChosenCItyID: $chosenIndex")
-        Toast.makeText(this, "Current chosen: $chosenIndex", Toast.LENGTH_SHORT).show()
-        return chosenIndex!!
+    override fun navigateToCitiesFragment() {
+        navigateTo(CitiesFragment.create(), addToBackstack = true)
     }
 }
