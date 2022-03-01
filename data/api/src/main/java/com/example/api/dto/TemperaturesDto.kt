71404c53716654485859214f8d061f90d7a39e6d
MainComp <sobolevvw@gmail.com>
2022-03-01 18:51:54 +0300

    added hour forecast to temperature dto

--- data/api/src/main/java/com/example/api/dto/TemperaturesDto.kt
+++ data/api/src/main/java/com/example/api/dto/TemperaturesDto.kt
@@ -1,5 +1,6 @@
 package com.example.api.dto
 
+import android.util.Log
 import com.example.forecast.domain.model.City
 import com.example.forecast.domain.model.CityWeather
 import com.example.forecast.domain.model.Daily
@@ -9,13 +10,14 @@ import java.util.*
 import kotlin.math.roundToInt
 
 data class TemperaturesDto(
+    @SerializedName("hourly") val hourlyTemp: List<HourlyTemp>,
     @SerializedName("daily") val dailyTemp: List<DailyTemp>,
 ) {
     fun toCityWeather(city: City): CityWeather {
         val result = ArrayList(dailyTemp.map {
             Daily(temp = it.temp.day.roundToInt() - 273, description = it.weather[0].main)
         })
-        return CityWeather(
+        val cityWeather = CityWeather(
             id = city.id,
             name = city.name,
             country = city.country,
@@ -25,11 +27,18 @@ data class TemperaturesDto(
             forecastDate = SimpleDateFormat("dd.MM.yyyy",
                 Locale.getDefault()).format(Calendar.getInstance().time),
         )
+        Log.d("HOUR", "toCityWeather: $hourlyTemp")
+        return cityWeather
     }
 }
 
 data class DailyTemp(
     val temp: Temp,
+    val weather: List<WeatherData>
+)
+
+data class HourlyTemp(
+    val temp: Double,
     val weather: List<WeatherData>
 )
 
