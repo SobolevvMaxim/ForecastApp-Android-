ee55a1d9bbd280fb06720aedcdb841c680db83a4
MainComp <sobolevvw@gmail.com>
2022-03-13 19:20:13 +0300

    domain module done

--- domain/src/main/java/com/example/forecast/domain/di/DomainModule.kt
+++ domain/src/main/java/com/example/forecast/domain/di/DomainModule.kt
@@ -1,4 +1,4 @@
-package com.example.forecast.di
+package com.example.forecast.domain.di
 
 import com.example.forecast.domain.repository.IForecastRepository
 import com.example.forecast.domain.use_case.*
